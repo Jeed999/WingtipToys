4c3a315d972e0cb035492b2d79085abfd8fde3f7
Jeed N <Jeed N>
2017-05-03 21:02:51 -0500

    ASP.NET 4.5 Web Form (VS 2017) - Wingtip Toys

---
+++ WingtipToys/Site.Master.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Models;
+
+
+namespace WingtipToys
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
+    }
+}
